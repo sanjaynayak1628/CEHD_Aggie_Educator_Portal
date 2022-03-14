ad77107b8f1ac6e63a24ae4bd0ad14dec3ca9c5b
Sanjay Nayak <sanjaynayakcs@gmail.com>
2022-03-14 16:03:05 -0500

    changed settings.py as per main branch

--- code/cehd/cehd/settings.py
+++ code/cehd/cehd/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'login.apps.LoginConfig',
+    'crispy_forms',
     'core',
     'epp_program',
     'epp_student',
