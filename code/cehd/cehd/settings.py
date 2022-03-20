564afdb135507f54160a1681a37fa03ff065cfbb
rajeshsatpathy1 <rajesh.a.s.1997@gmail.com>
2022-03-20 00:16:52 -0500

    Added static files

--- code/cehd/cehd/settings.py
+++ code/cehd/cehd/settings.py
@@ -139,7 +139,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = ''
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
