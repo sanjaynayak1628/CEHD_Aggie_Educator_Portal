d56b7c0ee1aed23cd3782e2174c3216b6e75d8e4
rajeshsatpathy1 <rajesh.a.s.1997@gmail.com>
2022-04-21 10:07:11 -0500

    Added supervisor view

--- code/cehd/supervisor/urls.py
+++ code/cehd/supervisor/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path, include
-
+from .views import SupervisorView
 
 
 urlpatterns = [
-    # Student save time sheet URL
-    # path('student/save/', CoopViewsSave.as_view(), name='coop_save_time_logs'),
-    # student submit time sheet URL
+    # Supervisor time sheets view URL
+    path('email/<str:email>/view/', SupervisorView.as_view(), name='supervisor_view_time_sheets'),
 ]
