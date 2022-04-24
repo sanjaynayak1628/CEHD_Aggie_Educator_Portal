3ceb5573a635c73f0069dc115726c4d5c99bb2e3
rajeshsatpathy1 <rajesh.a.s.1997@gmail.com>
2022-04-24 11:04:33 -0500

    Add supervisor view changes

--- code/cehd/supervisor/urls.py
+++ code/cehd/supervisor/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('email/<str:super_email>', SupervisorCoopView.as_view(), name='supervisor_coop_view'),
     path('email/<str:super_email>/coop/<str:coop_email>',
          SupervisorCoopGet.as_view(), name='supervisor_coop_get_time_logs'),
-    path('email/<str:super_email>/coop/<str:coop_email>/semester<str:semester>',
+    path('email/<str:super_email>/coop/<str:coop_email>/semester/<str:semester>',
          SupervisorCoopGet.as_view(), name='supervisor_coop_sem_get_time_logs'),
     path('email/<str:super_email>/coop/<str:coop_email>/semester/<str:semester>/year/<str:year>',
          SupervisorCoopGet.as_view(), name='supervisor_coop_sem_year_get_time_logs'),
